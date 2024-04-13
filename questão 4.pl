587f1d8e81f39d378a6590477cc2076bda1b7062
Fernanda Ferreira <114786782+fernanda-ferreira8@users.noreply.github.com>
2024-04-13 15:24:45 -0300

    correção feita vo tira um sonin já volto

--- questão 4.pl
+++ questão 4.pl
@@ -1,19 +1,21 @@
 %4) Implemente o fatorial de forma iterativa em Prolog.
 
-fatorial(0, 1). %Caso base, onde o fatorial de 0 é 1.
+fatorial(0, 1). % Caso base, onde o fatorial de 0 é 1.
 fatorial(N, F) :-
-    (   N > 0,
-        N1 is N - 1,
-        fatorial(N1, F1),
-    	F is N * F1
-    ).
+    N > 0,
+    N1 is N - 1,
+    fatorial(N1, F1),
+    F is N * F1.
 
-main:- 
-    (   
-    	write('Digite um numero para o calculo do fatorial'),
-        read(N), %N guarda o número do usuário
-    	fatorial(N,F),
-        write('Fatorial do numero inserido eh:'),
-        nl,
-    	write(F)
+main :-
+    repeat,
+    write('Entre com um numero (maior que 0) para calcular seu fatorial: '),
+    read(N),
+    (   N >= 0 ->
+        fatorial(N, F),
+        write('O fatorial do numero digitado é '), 
+        write(F), nl,
+        ! ;    % é o "corte" que para o loop
+        write('Número inválido! Por favor, digite novamente.'), nl,
+        fail % fail é usado para continuar com o loop
     ).
