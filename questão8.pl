ca24342592ce232b8d74d15b53bdb954cfbc2486
Fernanda Ferreira <114786782+fernanda-ferreira8@users.noreply.github.com>
2024-04-14 18:49:24 -0300

    no meu funciona

---
+++ questão8.pl
@@ -0,0 +1,19 @@
+
+ler(Nome) :- repeat,
+    read(N),
+    (N == end_of_file -> !;
+     read(I),
+     read(S),
+     read(_),
+     (N == Nome ->( write(I), write(' '), write(S), !);
+     fail)).
+
+abrir() :-
+    open('C:/Users/ferre/Desktop/2023.2/Programação Lógica/Trabalho Final/arquivos.txt', read, X),
+    write('Insira um nome: '),
+    read(Nome),
+    set_input(X),
+    ler(Nome),
+    set_input(user),
+    close(X).
+
